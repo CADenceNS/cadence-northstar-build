be5e9135c40f52ce5d03e1912f1b64301dff8d7e
CADenceNS <dorianhabet@yahoo.com>
2026-07-26 16:55:02 -0700

    Mount authenticated Sprint 3 shell

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { Sprint3App } from './Sprint3App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Sprint3App />
   </React.StrictMode>
 );
