ad12a1619b949ec701607487ac8f67adfd77c6f2
CADenceNS <dorianhabet@yahoo.com>
2026-07-26 20:06:30 -0700

    feat(qc): launch Sprint 6 application

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Sprint5App } from './Sprint5App';
+import { Sprint6App } from './Sprint6App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Sprint5App />
+    <Sprint6App />
   </React.StrictMode>
 );
