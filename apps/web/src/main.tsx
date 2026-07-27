ba7df55afef13d57c0bb978180e826e30bc3bf4d
CADenceNS <dorianhabet@yahoo.com>
2026-07-26 21:35:10 -0700

    feat(finance): launch Sprint 8 application shell

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Sprint7App } from './Sprint7App';
+import { Sprint8App } from './Sprint8App';
 import './styles.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Sprint7App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><Sprint8App /></React.StrictMode>);
