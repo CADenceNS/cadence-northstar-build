c700580d2f9da44ff9e466e2c67c97767cea86d3
CADenceNS <dorianhabet@yahoo.com>
2026-07-27 12:04:19 -0700

    Initialize secure browser session transport

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './security-client';
 import { Sprint8App } from './Sprint8App';
 import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><Sprint8App /></React.StrictMode>);
