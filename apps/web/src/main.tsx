47cf2c77025f710facface8e609c6a335d354984
CADenceNS <dorianhabet@yahoo.com>
2026-07-26 18:02:39 -0700

    Launch authenticated Sprint 4 application

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Sprint3App } from './Sprint3App';
+import { Sprint4App } from './Sprint4App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Sprint3App />
+    <Sprint4App />
   </React.StrictMode>
 );
