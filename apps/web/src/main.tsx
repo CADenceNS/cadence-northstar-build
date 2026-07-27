dfaa6094c8e9e300a5332738918c27af1a9cd4ba
CADenceNS <dorianhabet@yahoo.com>
2026-07-26 20:48:13 -0700

    feat(shipping): boot Sprint 7 application

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Sprint6App } from './Sprint6App';
+import { Sprint7App } from './Sprint7App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Sprint6App />
+    <Sprint7App />
   </React.StrictMode>
 );
