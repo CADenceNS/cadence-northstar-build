7a9ee098e23f0f1d9ec051512b573c70d3dcaa11
CADenceNS <dorianhabet@yahoo.com>
2026-07-28 15:33:23 -0700

    Add intake administration rollback

---
+++ apps/api/migrations/0006_intake_administration.rollback.sql
@@ -0,0 +1,6 @@
+BEGIN;
+DROP TABLE IF EXISTS pricing_schedule_items;
+DROP TABLE IF EXISTS pricing_schedules;
+DROP TABLE IF EXISTS tenant_routing_defaults;
+DROP TABLE IF EXISTS practice_routing_profiles;
+COMMIT;
