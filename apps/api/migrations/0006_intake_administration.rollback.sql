ebc41a3b03d04e88a830869fe59fea7bbf6cf38c
CADenceNS <dorianhabet@yahoo.com>
2026-07-28 15:49:10 -0700

    Restore catalog columns during administration rollback

--- apps/api/migrations/0006_intake_administration.rollback.sql
+++ apps/api/migrations/0006_intake_administration.rollback.sql
@@ -3,4 +3,7 @@ DROP TABLE IF EXISTS pricing_schedule_items;
 DROP TABLE IF EXISTS pricing_schedules;
 DROP TABLE IF EXISTS tenant_routing_defaults;
 DROP TABLE IF EXISTS practice_routing_profiles;
+ALTER TABLE product_catalog
+  ADD COLUMN IF NOT EXISTS default_customer_price numeric(12,2),
+  ADD COLUMN IF NOT EXISTS promotional_price numeric(12,2);
 COMMIT;
